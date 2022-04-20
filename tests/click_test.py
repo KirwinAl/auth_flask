5b37816ce5243fd7aede5ce9a32780e296440427
KirwinAl <89887144+KirwinAl@users.noreply.github.com>
2022-04-19 22:33:12 -0400

    fixes the import error

--- tests/click_test.py
+++ tests/click_test.py
@@ -22,6 +22,36 @@ def test_error_log_file():
     logfile = os.path.exists(errorLog)
     assert logfile == True
 
+def test_flask_log_file():
+    root = os.path.dirname(os.path.abspath(__file__))
+    flaskLog = os.path.join(root, '../app/logs/flask.log')
+    logfile = os.path.exists(flaskLog)
+    assert logfile == True
+
+def test_myapp_log_file():
+    root = os.path.dirname(os.path.abspath(__file__))
+    myappLog = os.path.join(root, '../app/logs/myapp.log')
+    logfile = os.path.exists(myappLog)
+    assert logfile == True
+
+def test_request_log_file():
+    root = os.path.dirname(os.path.abspath(__file__))
+    requestLog = os.path.join(root, '../app/logs/request.log')
+    logfile = os.path.exists(requestLog)
+    assert logfile == True
+
+def test_sql_log_file():
+    root = os.path.dirname(os.path.abspath(__file__))
+    sqlLog = os.path.join(root, '../app/logs/sqlalchemy.log')
+    logfile = os.path.exists(sqlLog)
+    assert logfile == True
+
+def test_werk_log_file():
+    root = os.path.dirname(os.path.abspath(__file__))
+    werkLog = os.path.join(root, '../app/logs/werkzeug.log')
+    logfile = os.path.exists(werkLog)
+    assert logfile == True
+
 def test_create_database():
     response = runner.invoke(create_database)
     assert response.exit_code == 0
